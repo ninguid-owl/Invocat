ad25637b4351ae16ab54beba45e61f61c2523f92
Christopher Antoun <christopher@antoun.org>
2017-09-17 14:12:14 -0400

    Document Interpreter

--- Sources/Interpreter.swift
+++ Sources/Interpreter.swift
@@ -3,19 +3,24 @@
 //  LibInvocat
 //
 
+/// An interpreter for the Invocat language.
 class Interpreter {
     let parser: Parser
     let evaluator: Evaluator
 
+    /// The interpreter keeps the state against which it evaluates expressions.
     var state: InvState = [:]
 
+    /// Initializes an Interpreter with a given random seed.
     init(seed: String? = nil) {
         parser = Parser()
         evaluator = seed == nil ? Evaluator() : Evaluator(seed: seed!)
     }
 
-    // TODO Test
-    // NOTE side effects: updates state!
+    /// Evaluates a list of expressions in the interpreter's current state, 
+    /// updating that state each time.
+    ///
+    /// Note: This function has the side effect of updating state.
     private func evaluate(_ exps: [InvExp]) -> [String]? {
         let values = exps.flatMap { (exp: InvExp) -> String? in
             let value: String?
@@ -25,6 +30,13 @@ class Interpreter {
         return values.isEmpty ? nil : values
     }
 
+    /// Interprets a String of Invocat source text and returns an optional array
+    /// of any resulting values.
+    ///
+    /// The text is tokenized, parsed, and then evaluated against the
+    /// interpreter's state.
+    ///
+    /// - Parameter text: A String of Invocat source.
     func eval(text: String) -> [String]? {
         let tokens = Lexer.tokens(from: text)
         let expressions = parser.parse(tokens: tokens)
@@ -32,6 +44,13 @@ class Interpreter {
         return values
     }
 
+    /// Interprets a file of Invocat source text and returns an optional array
+    /// of any resulting values.
+    ///
+    /// The text is tokenized, parsed, and then evaluated against the
+    /// interpreter's state.
+    ///
+    /// - Parameter file: A path to an Invocat source file.
     func eval(file path: String) -> [String]? {
         guard let text = try? String(contentsOfFile: path) else { return nil }
         return eval(text: text)
