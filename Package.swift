545d20af8e869d42f0acd8794f3527332a2518ff
Matthew Antoun <matthew@antoun.org>
2017-10-01 16:58:28 -0400

    Configure package to produce a library and executable

--- Package.swift
+++ Package.swift
@@ -3,21 +3,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "LibInvocat",
+    name: "Invocat",
     products: [
-        .library(
-            name: "LibInvocat",
-            targets: ["LibInvocat"]),
+        .library(name: "LibInvocat", targets: ["LibInvocat"]),
+        .executable(name: "invocat", targets: ["invocat"])
     ],
     targets: [
-        .target(
-            name: "LibInvocat",
-            path: "Sources"),
-        .testTarget(
-            name: "LibInvocatTests",
-            dependencies: ["LibInvocat"],
-            path: "Tests")
+        .target(name: "LibInvocat"),
+        .target(name: "invocat", dependencies: ["LibInvocat"]),
+        .testTarget(name: "LibInvocatTests", dependencies: ["LibInvocat"])
     ]
 )
 
+// Requires macOS 10.11 or newer:
 // swift build -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.11"
