fcea9495b032ca3da470d31e267ec3af612bac6d
Matthew Antoun <matthew@antoun.org>
2017-10-01 14:36:53 -0400

    Update package description

--- Package.swift
+++ Package.swift
@@ -3,5 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "LibInvocat"
+    name: "LibInvocat",
+    products: [
+        .library(
+            name: "LibInvocat",
+            targets: ["LibInvocat"]),
+    ],
+    targets: [
+        .target(
+            name: "LibInvocat",
+            path: "Sources"),
+        .testTarget(
+            name: "LibInvocatTests",
+            dependencies: ["LibInvocat"],
+            path: "Tests")
+    ]
 )
+
+// swift build -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.11"
