dc64d8648ba5a23a556f85ae402baff889113294
Matthew Antoun <matthew@antoun.org>
2017-10-05 21:50:49 -0400

    Capitalization

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ let package = Package(
     name: "Invocat",
     products: [
         .library(name: "LibInvocat", targets: ["LibInvocat"]),
-        .executable(name: "invocat", targets: ["invocat"])
+        .executable(name: "invocat", targets: ["Invocat"])
     ],
     targets: [
         .target(name: "LibInvocat"),
-        .target(name: "invocat", dependencies: ["LibInvocat"]),
+        .target(name: "Invocat", dependencies: ["LibInvocat"]),
         .testTarget(name: "LibInvocatTests", dependencies: ["LibInvocat"])
     ]
 )
 
 // Requires macOS 10.11 or newer:
-// swift build -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.11"
+// swift build -Xswiftc -target -Xswiftc x86_64-apple-macosx10.11
